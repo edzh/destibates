c224fc3f4deabb87be6831983dbc8e1b6fd72c2f
edwinzzhou@gmail.com <edwinzzhou@gmail.com>
2018-09-28 21:35:55 -0400

    Minor changes to css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () =>
-  <div className="p-6 mb-4 bg-grey-darker">
+  <div className="p-6 mb-4 shadow-md bg-grey-darker">
     <h1 className="text-white mr-6">Destibates</h1>
   </div>
 
