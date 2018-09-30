99c0b29a7e5bc3fe1c3e556b38a49aa0cf53d9de
edwinzzhou@gmail.com <edwinzzhou@gmail.com>
2018-09-30 16:39:19 -0400

    Fix routing

--- src/App.js
+++ src/App.js
@@ -4,24 +4,20 @@ import videoIds from './data/videoIds';
 
 import Player from './components/Player';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div className="App font-sans">
-        <Navbar />
-        <div>
-          <Route path={'/v/:videoId/:timestamp'} component={Player} />
-          <Route path={'/'} render={() =>
-            <div>
-              <Link className=" no-underline" to={'/v/311225842/0:00:00'}><button className="block text-white mx-auto mt-8 hover:bg-grey-lighter hover:text-grey-darkest rounded bg-grey-darker font-semibold text-lg p-4 shadow-md">Click here to begin watching</button></Link>
-            </div>
-          }
-          />
+        <div className="App font-sans">
+          <Navbar />
+          <div>
+            <Route path={'/v/'} component={Player} />
+            <Route exact path={'/'} component={Home} />
+          </div>
         </div>
-      </div>
       </BrowserRouter>
     );
   }
