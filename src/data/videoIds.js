8c2f27d79f1702d5001a4403cf80b04e63782ff7
edwinzzhou@gmail.com <edwinzzhou@gmail.com>
2018-09-30 21:02:19 -0400

    Add LWoD September 30 2018

--- src/data/videoIds.js
+++ src/data/videoIds.js
@@ -4,63 +4,63 @@ const videoIds = {
     timestamps: {
       "0:00:00" : {
         "topic": "Talking about Police shooting vs obesity inconsistency post on the subreddit",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "0:02:04": {
         "topic": "Discussing livestreamfails with MrMouton",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "0:08:39": {
         "topic": "Last day of the beta with MrMouton",
-        "category": "COD:BO4 Blackout",
+        "category": "COD:BO4 Blackout"
       },
       "2:12:24": {
         "topic": "Devin talks to MrMouton about weight loss/games/chat while Destiny goes afk",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "2:23:25": {
         "topic": "Talking about the biggest loser study and weight loss with MrMouton and Devin",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "2:35:21": {
         "topic": "Solo games of Blackout",
-        "category": "COD:BO4 Blackout",
+        "category": "COD:BO4 Blackout"
       },
       "3:08:05": {
         "topic": "Destiny and Devin ordering food",
-        "category": "-",
+        "category": "-"
       },
       "3:16:28": {
         "topic": "Watching videos from /r/videos and /r/livestreamfails ",
-        "category": "Videos",
+        "category": "Videos"
       },
       "3:20:24": {
         "topic": "Solo games of Blackout",
-        "category": "COD:BO4 Blackout",
+        "category": "COD:BO4 Blackout"
       },
       "4:14:07": {
         "topic": "Watching videos about David Blaine with Devin while eating and general magic talk",
-        "category": "Videos",
+        "category": "Videos"
       },
       "4:56:48": {
         "topic": "Talking about eSports and donations with Devin",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "5:04:38": {
         "topic": "Talking to Vigor and Pokelawls about bronies and furries ",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "5:07:55": {
         "topic": "Vigor has a probelm ejaculating and gets a lesson on kidney stones and human anatomy",
-        "category": "Discussion",
+        "category": "Discussion"
       },
       "5:15:56": {
         "topic": "Trainwrecks joins the call and does a reddit recap of /r/livestreamfails with Destiny",
-        "category": "Videos",
+        "category": "Videos"
       },
       "5:24:52": {
         "topic": "Squad games with Pokelawls, Trainwrecks and Vigor",
-        "category": "COD:BO4 Blackout",
+        "category": "COD:BO4 Blackout"
       },
     }
   },
@@ -691,6 +691,577 @@ const videoIds = {
       "1:59:02": {
         topic: "Playing Starcraft 2 (audio partially muted due to copyright)",
         category: "Starcraft 2"
+      }
+    }
+  },
+  313960050: {
+    date: "09/24/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Starcraft 2",
+        category: "Learning/laddering as Protoss (Audio partially muted)",
+      },
+      "1:24:29": {
+        topic: "Discussion",
+        category: "Drake talking to a 14 year old",
+      },
+      "1:28:06": {
+        topic: "Discussion",
+        category: "New Brett Kavanaugh accusations",
+      },
+      "1:29:26": {
+        topic: "Discussion",
+        category: "More Drake stuff",
+      },
+      "1:31:42": {
+        topic: "Discussion",
+        category: "Weight lifting meme",
+      },
+      "1:35:09": {
+        topic: "Discussion",
+        category: "Talking about childing grooming",
+      },
+      "1:36:59": {
+        topic: "Videos",
+        category: "Two memes from the subreddit",
+      },
+      "1:38:23": {
+        topic: "Video/Discussion",
+        category: "Watching a Joe Rogan video on income inequality",
+      },
+      "1:40:44": {
+        topic: "Videos",
+        category: "Two twitch clips ",
+      },
+      "1:42:57": {
+        topic: "Hollow Knight",
+        category: "Playing Hollow Knight",
+      },
+      "1:47:27": {
+        topic: "Hollow Knight/Video",
+        category: "Listening to a Sam Harris debate about moral realism while playing Hollow Knight",
+      },
+      "2:26:41": {
+        topic: "Hollow Knight",
+        category: "Playing Hollow Knight",
+      },
+      "2:29:35": {
+        topic: "Hollow Knight/Discussion",
+        category: "Talking about personal values/suicide while playing Hollow Knight",
+      },
+      "2:35:17": {
+        topic: "-",
+        category: "Break (Audio partially muted)",
+      },
+      "2:45:21": {
+        topic: "Videos",
+        category: "Memes from the chat",
+      },
+      "2:49:19": {
+        topic: "Hollow Knight",
+        category: "Playing Hollow Knight",
+      },
+      "2:54:19": {
+        topic: "Discussion",
+        category: "Talking about the technicalities of twitch chat and dgg chat",
+      },
+      "2:55:57": {
+        topic: "Hollow Knight/Discussion",
+        category: "More chat talk and Hollow Knight",
+      },
+      "2:57:45": {
+        topic: "Hollow Knight",
+        category: "Reading comments from chat and playing Hollow Knight",
+      },
+      "3:26:25": {
+        topic: "Hollow Knight/Discussion",
+        category: "Planning a trip to Finland ",
+      },
+      "3:37:12": {
+        topic: "Discussion",
+        category: "Reading a post from the Sam Harris subreddit",
+      },
+      "3:40:03": {
+        topic: "Discussion",
+        category: "Checking out how Andy Warski/Tonka Trucks are doing on Youtube",
+      },
+      "3:46:38": {
+        topic: "Hollow Knight/Talking",
+        category: "Natsumii joins discord for a chat about games/twitch while playing Hollow Knight",
+      },
+      "4:13:42": {
+        topic: "Hollow Knight/Discussion",
+        category: "More Brett Kavanaugh accusations",
+      },
+      "4:17:58": {
+        topic: "Hollow Knight/Discussion",
+        category: "Discussion on Rape Culture",
+      },
+      "4:21:59": {
+        topic: "Hollow Knight",
+        category: "Reading comments from chat and playing Hollow Knight",
+      },
+      "4:43:02": {
+        topic: "Videos",
+        category: "Watching a video on NA cops ",
+      },
+      "4:55:59": {
+        topic: "Videos/Discussion",
+        category: "Reddit post and on Jordon Peterson talking about women in the work force",
+      },
+      "5:09:02": {
+        topic: "-",
+        category: "Break ",
+      },
+      "5:33:09": {
+        topic: "Videos",
+        category: "Livestreamfails clips",
+      },
+      "5:40:09": {
+        topic: "Discussion",
+        category: "Tinder meta",
+      },
+      "5:41:24": {
+        topic: "Discussion",
+        category: "Looking at a race realists reddit profile",
+      },
+      "5:45:23": {
+        topic: "Video",
+        category: "Black Ops meme",
+      },
+      "5:46:41": {
+        topic: "Discussion",
+        category: "Talking about instagram ",
+      },
+      "5:49:18": {
+        topic: "-",
+        category: "Break",
+      },
+      "5:51:20": {
+        topic: "Hollow Knight",
+        category: "Playing Hollow Knight",
+      },
+      "6:06:26": {
+        topic: "Hollow Knight/Discussion",
+        category: "Talking about psychedelics/panic attacks/existence while playing Hollow Knight",
+      },
+      "6:48:55": {
+        topic: "-",
+        category: "Break",
+      },
+      "6:54:23": {
+        topic: "-",
+        category: "Sorting out website server stuff",
+      },
+      "6:55:39": {
+        topic: "Hollow Knight",
+        category: "Playing Hollow Knight and reading messages from chat",
+      },
+      "7:10:15": {
+        topic: "Hollow Knight/Video",
+        category: "Listening to a Sargon video talking about UKIP and playing Hollow Knight",
+      },
+      "7:20:59": {
+        topic: "Discussion",
+        category: "Looking at Youtube analytics",
+      },
+      "7:26:14": {
+        topic: "Hollow Knight/Video",
+        category: "Listening to a Sargon video talking about UKIP and playing Hollow Knight",
+      },
+      "7:44:27": {
+        topic: "Hollow Knight",
+        category: "Playing Hollow Knight",
+      }
+    }
+  },
+  314321361: {
+    date: "09/24/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Mic muted",
+        category: "-"
+      },
+      "0:05:20": {
+        topic: "Looking and talking at stocks",
+        category: "Discussion"
+      },
+      "0:09:50": {
+        topic: "Bill Burr talking about Louis CK returning to comedy",
+        category: "Video"
+      },
+      "0:11:24": {
+        topic: "Reading reddit posts",
+        category: "Reddit"
+      },
+      "0:11:51": {
+        topic: "Talking about social media platforms",
+        category: "Discussion"
+      },
+      "0:12:24": {
+        topic: "Couple of memes from reddit",
+        category: "Reddit"
+      },
+      "0:18:04": {
+        topic: "Playing Protoss in Starcraft 2",
+        category: "Starcraft 2"
+      },
+      "1:25:34": {
+        topic: "Reading a twitter thread and looking at reddit",
+        category: "Reddit/Twitter"
+      },
+      "1:26:59": {
+        topic: "Talking about netflix",
+        category: "Discussion"
+      },
+      "1:29:00": {
+        topic: "Playing Hollow Knight and talking to Rem about philosophy",
+        category: "Hollow Knight/Discussion"
+      },
+      "2:36:33": {
+        topic: "Looking at twitter and a livestreamfails clip",
+        category: "Twitter/Videos"
+      },
+      "2:40:45": {
+        topic: "Break",
+        category: "-"
+      },
+      "2:43:44": {
+        topic: "Article on linux code of conduct",
+        category: "Article"
+      },
+      "2:44:30": {
+        topic: "Looking for a stream to host",
+        category: "-",
+      }
+    }
+  },
+  314434914: {
+    date: "09/25/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Break",
+        category: "-"
+      },
+      "0:06:47": {
+        topic: "Who was better Backstreet Boys or Nsync",
+        category: "Discussion"
+      },
+      "0:07:54": {
+        topic: "Watching a video about a guy who took 30 tabs of LSD and talking about LSD",
+        category: "Video"
+      },
+      "0:12:13": {
+        topic: "League with Lily/Janet/Toast/Kelvin",
+        category: "League of Legends (Squad)"
+      },
+      "1:00:00": {
+        topic: "Playing CS:GO with MrMouton",
+        category: "CS:GO (Duo)"
+      },
+      "1:30:13": {
+        topic: "Destiny gets kicked for team damage",
+        category: "CS:GO (Duo)"
+      },
+      "1:30:35": {
+        topic: "Reading a post on psychedelics ",
+        category: "Reddit"
+      },
+      "1:33:18": {
+        topic: "Clips from livestreamfails and Youtube while MrMouton finishes his game",
+        category: "Videos"
+      },
+      "1:47:11": {
+        topic: "PUBG with MrMouton (Stream cuts out)",
+        category: "PUBG (Duo)"
+      }
+    }
+  },
+  314464306: {
+    date: "09/25/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "PUBG with MrMouton ",
+        category: "PUBG (Duo)"
+      },
+      "0:10:38": {
+        topic: "Man playing violin",
+        category: "Video"
+      },
+      "0:11:44": {
+        topic: "Chess with MrMouton",
+        category: "Chess"
+      },
+      "0:35:34": {
+        topic: "Reading twitter and talking to MrMouton",
+        category: "Twitter"
+      },
+      "0:38:57": {
+        topic: "Video on Star Wars",
+        category: "Video"
+      },
+      "0:39:55": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "1:26:11": {
+        topic: "Looking for a stream to host",
+        category: "-"
+      }
+    }
+  },
+  314651155: {
+    date: "09/25/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "0:19:55": {
+        topic: "Destiny trying to find out his most played game on steam",
+        category: "-"
+      },
+      "0:23:15": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "0:50:54": {
+        topic: "Trash meme video",
+        category: "Video"
+      },
+      "0:51:43": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "0:54:10": {
+        topic: "Livestreamfails clip",
+        category: "Video"
+      },
+      "0:55:00": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "1:04:04": {
+        topic: "Destiny gets jebaited",
+        category: "Meme"
+      },
+      "1:05:21": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "1:19:00": {
+        topic: "Break",
+        category: "-"
+      },
+      "1:26:35": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      }
+    }
+  },
+  314706093: {
+    date: "09/25/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "2:01:12": {
+        topic: "Watching a livestream of Will Smith bungee jumping out of a helicopter",
+        category: "Video"
+      },
+      "2:03:21": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "2:09:26": {
+        topic: "Break",
+        category: "-"
+      },
+      "2:21:32": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "4:00:42": {
+        topic: "Break",
+        category: "-"
+      },
+      "4:03:17": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "4:25:02": {
+        topic: "Reading an article on Michael Avenatti",
+        category: "Article"
+      },
+      "4:29:51": {
+        topic: "Playing Hollow Knight and talking to MrMouton",
+        category: "Hollow Knight"
+      },
+      "6:36:41": {
+        topic: "Playing CS:GO with MrMouton",
+        category: "CS:GO (DUO)"
+      },
+      "7:14:24": {
+        topic: "Playing chess with MrMouton",
+        category: "Chess"
+      },
+      "7:41:31": {
+        topic: "Short chess meme video",
+        category: "Video"
+      },
+      "7:41:47": {
+        topic: "Trump twitter meme",
+        category: "Twitter"
+      },
+      "7:43:10": {
+        topic: "Reading about the Black Ops release date",
+        category: "-"
+      }
+    }
+  },
+  315654126: {
+    date: "09/28/2018"
+  },
+  316369054: {
+    date: "09/29/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Livestreamfails clips",
+        category: "Videos"
+      },
+      "0:06:11": {
+        topic: "Playing Hollow Knight",
+        category: "Hollow Knight"
+      },
+      "0:07:17": {
+        topic: "Talking about Instagram ",
+        category: "Discussion"
+      },
+      "0:09:53": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "0:17:12": {
+        topic: "Reading a /r/destiny post about philosophy ",
+        category: "Reddit"
+      },
+      "0:19:20": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "0:21:33": {
+        topic: "Playing Hollow Knight and talking about the Dr. Ford/Kavanaugh hearing",
+        category: "Hollow Knight/Discussion"
+      },
+      "0:23:56": {
+        topic: "Talking about the Dr. Ford/Kavanaugh hearing",
+        category: "Discussion"
+      },
+      "0:26:46": {
+        topic: "Playing Hollow Knight and talking about the Dr. Ford/Kavanaugh hearing",
+        category: "Hollow Knight/Discussion"
+      },
+      "0:29:35": {
+        topic: "Reading an article on Al Franken",
+        category: "Article"
+      },
+      "0:31:53": {
+        topic: "Talking about the Dr. Ford/Kavanaugh hearing",
+        category: "Discussion"
+      },
+      "0:41:54": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "0:43:16": {
+        topic: "Playing Hollow Knight and listening to Raj Patel podcast",
+        category: "Hollow Knight/Video"
+      },
+      "0:48:00": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "1:32:29": {
+        topic: "Talking about anime/Death Note and playing Hollow Knight",
+        category: "Hollow Knight/Discussion"
+      },
+      "1:44:33": {
+        topic: "Talking about weight loss and playing Hollow Knight",
+        category: "Hollow Knight/Discussion"
+      },
+      "1:59:29": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "2:29:56": {
+        topic: "Talking to chatter about Sam Harris and playing Hollow Knight",
+        category: "Hollow Knight/Discussion"
+      },
+      "2:58:37": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "3:20:43": {
+        topic: "Listening to some dumb fuck internet bloodsport stream and playing Hollow Knight",
+        category: "Hollow Knight/Video"
+      },
+      "3:35:20": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight/Discussion"
+      },
+      "3:39:49": {
+        topic: "Playing Hollow Knight and listening to a Sam Harris debate",
+        category: "Hollow Knight"
+      },
+      "4:03:56": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "4:28:29": {
+        topic: "Break",
+        category: "-"
+      },
+      "4:32:07": {
+        topic: "Playing Hollow Knight and reading comments from chat",
+        category: "Hollow Knight"
+      },
+      "5:32:45": {
+        topic: "Finding a stream to host",
+        category: "-"
+      }
+    }
+  },
+  316526398: {
+    date: "09/30/2018",
+    timestamps: {
+      "0:00:00": {
+        topic: "Videos from livestreamfails",
+        category: "Videos"
+      },
+      "0:06:53": {
+        topic: "Laddering as zerg (partially muted due to copyright)",
+        category: "Starcraft 2"
+      },
+      "0:24:41": {
+        topic: "Playing League with Lily, Fed and Toast.",
+        category: "League of Legends"
+      },
+      "2:03:16": {
+        topic: "Videos from livestreamfails",
+        category: "Videos"
+      },
+      "2:05:36": {
+        topic: "Looking at tesla and facebook stocks",
+        category: "Discussion"
+      },
+      "2:06:47": {
+        topic: "Trump memes",
+        category: "Videos"
+      },
+      "2:07:56": {
+        topic: "Reading a post about Elon/Musk and Tesla",
+        category: "Reddit"
       }
     }
   }
