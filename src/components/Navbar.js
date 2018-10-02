2c78e70ac333d03bdf2f81ae29fdd9b540f0dbc1
Edwin Zhou <edwinzzhou@gmail.com>
2018-10-02 12:36:18 -0400

    Add options tab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () =>
   <div className="p-6 mb-4 shadow-md bg-grey-darker">
     <h1 className="text-white mr-6">Destibates</h1>
+    <NavLink to="/options" ><p>Options</p></NavLink>
   </div>
 
 
